0461e683f5b6ffabcfaf14979b50eb5bd582cd4b
Rohan Huynh <rohan.huynh@mrjaffesclass.com>
2023-09-08 11:34:20 -0700

    DataTypes

--- Datatypes.java
+++ Datatypes.java
@@ -1,10 +1,11 @@
-
-
-public class HelloWorld 
+public class Datatypes 
 {
     public static void main(String[] args) 
     {
         System.out.println("Hello World!") ;
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
     }
 }
 
